64fa45b032d0494036161881fa9743d316be6ccd
FRabbi <mdfazlerabbi.diu@gmail.com>
2022-02-13 17:56:53 +0600

    simple event binding

---
+++ src/app/page1/page1.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page1',
+  templateUrl: './page1.component.html',
+  styleUrls: ['./page1.component.css']
+})
+export class Page1Component implements OnInit {
+  pageName = 'PAGE 1'
+
+  constructor() { }
+
+  ngOnInit(): void {
+    setTimeout(()=>{
+      this.pageName = 'Hey I came';
+    },5000)
+  }
+
+  onButtonClick():void{
+    alert("Today::: "+ new Date());
+  }
+
+}
