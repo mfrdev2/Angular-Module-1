fc79e0a94cccbe2a4257862a8af94a27db124220
FRabbi <mdfazlerabbi.diu@gmail.com>
2022-02-14 11:04:49 +0600

    child property access from parent property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,11 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-first-project';
+  @ViewChild("footer")
+  footer!: FooterComponent;
+
+  updateLastAccess():void{
+    this.footer.lastAccess = new Date().toDateString();
+  }
 }
 
